2c71c87a595188f73a9dbd8fe174ede75c8769e7
Андрій <gapeev1979@gmail.com>
2025-08-08 11:41:45 +0300

    fixing bugs in MovieGrid

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -2,23 +2,23 @@ import css from './MovieGrid.module.css';
 import { type Movie } from '../../types/movie';
 
 interface MovieGridProps {
-  onSelect: (movieId: Movie) => void;
+  onSelect: (movie: Movie) => void;
   movies: Movie[];
 }
 
 export default function MovieGrid({ onSelect, movies }: MovieGridProps) {
   return (
     <ul className={css.grid}>
-      {movies.map(movies => (
-        <li key={movies.id} onClick={() => onSelect(movies)}>
+      {movies.map(movie => (
+        <li key={movie.id} onClick={() => onSelect(movie)}>
           <div className={css.card}>
             <img
               className={css.image}
-              src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
-              alt={movies.title}
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
               loading="lazy"
             />
-            <h2 className={css.title}>{movies.title}</h2>
+            <h2 className={css.title}>{movie.title}</h2>
           </div>
         </li>
       ))}
